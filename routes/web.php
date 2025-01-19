6c36ad8556f3b9130ad6510952800dbcf90b515e
Vinícius Yoda <vyoda4604@gmail.com>
2025-01-19 10:17:37 -0300

    rotas

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,26 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/empresa', function() {
-    return view('site/empresa');
+// Route::any('/any', function() {
+//     return "Perminte todo tipo de acesso http";
+// });
+
+// Route:match(['get', 'post'], '/match', function() {
+//     return "Perminte apenas acessos definidos";
+// });
+
+Route::get('/produto/{id}/{cat?}', function($id, $cat = "") {
+    return "O id do produto é:".$id."<br>"."A categoria do produto é:".$cat;
+});
+
+Route::redirect('/sobre', '/empresa');
+
+Route::view('/empresa', 'site/empresa');
+
+Route::get('/timesnownews', function() {
+    return view('news');
+})->name('noticias');
+
+Route::get('/novidades', function() {
+    return redirect()->route('noticias');
 });
