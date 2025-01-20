e66eadb61481f090ea54252b13b92d206272cb8d
Vinícius Yoda <vyoda4604@gmail.com>
2025-01-20 10:26:56 -0300

    Controller

--- routes/web.php
+++ routes/web.php
@@ -1,42 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProdutoController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::resource('produtos', ProdutoController::class);
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-// Route::any('/any', function() {
-//     return "Perminte todo tipo de acesso http";
-// });
 
-// Route:match(['get', 'post'], '/match', function() {
-//     return "Perminte apenas acessos definidos";
-// });
 
-Route::get('/produto/{id}/{cat?}', function($id, $cat = "") {
-    return "O id do produto é:".$id."<br>"."A categoria do produto é:".$cat;
-});
 
-Route::redirect('/sobre', '/empresa');
-
-Route::view('/empresa', 'site/empresa');
-
-Route::get('/timesnownews', function() {
-    return view('news');
-})->name('noticias');
-
-Route::get('/novidades', function() {
-    return redirect()->route('noticias');
-});
