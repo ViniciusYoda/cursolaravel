de445c875fc64bb11843c8d3e4ec2cdafb3578a0
Vinícius Yoda <vyoda4604@gmail.com>
2025-01-22 10:02:39 -0300

    Model

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Produto;
 
 class ProdutoController extends Controller
 {
@@ -11,7 +12,8 @@ class ProdutoController extends Controller
      */
     public function index()
     {
-        //
+        $produtos = Produto::all();
+        return dd($produtos);
     }
 
     /**
